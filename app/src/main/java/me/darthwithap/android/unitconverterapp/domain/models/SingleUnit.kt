7c79d0ff5f218de4022de8a9d3345599955ffbd0
patthhar <parthdroidworld@gmail.com>
2023-08-04 00:48:03 +0530

    Added symbol to SingleUnit model

--- app/src/main/java/me/darthwithap/android/unitconverterapp/domain/models/SingleUnit.kt
+++ app/src/main/java/me/darthwithap/android/unitconverterapp/domain/models/SingleUnit.kt
@@ -3,6 +3,7 @@ package me.darthwithap.android.unitconverterapp.domain.models
 data class SingleUnit(
   val id: Int,
   val name: String,
+  val symbol: String,
   val multiplier: Float,
   val offset: Float
 ) {
